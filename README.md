cab3f9521401d5e3fdacd259bcd0e8e99575b7e3
Erin Turnbull <erin.a.turnbull@gmail.com>
2022-06-14 11:00:25 -0400

    Renamed package because it wasn't accepted on pypi

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
-# PackTest
+# Entry Point Testing
 
 This module is designed to provide a strictly in-code implementation of MetaPathFinder that can be leveraged in unit
 testing in order to simulate an entry point being created, as would be loaded by `importlib.metadata.entry_points()` 
 in Python 3.8+ or by `importlib_metadata.entry_points()` in earlier versions. 
 
-If there are requests, additional functionality aimed at testing plugin packages may be added in future releases.
+Requests for additional functionality aimed at testing plugin packages will be considered.
 
 
 ## Usage
+
 ```python
 import unittest
-import packtest
+import eptest
+
 
 class Test(unittest.TestCase):
 
     def test_me(self):
         # Could move some of this to be a test fixture instead
-        
+
         # Finder on sys.meta_path
-        finder = packtest.TestFinder()
-        
+        finder = eptest.TestFinder()
+
         # Add it to sys.meta_path
         finder.register()
-        
+
         # Create a package
-        package = packtest.TestPackage("foobar")
-        
+        package = eptest.TestPackage("foobar")
+
         # Create entry points
         package.add_entry_point(
             # Equivalent to a module with the following in setup.cfg:
             # [options.entry_points]
             # myep.group = 
             #     foobar = myep.tests:test.attr  
-            packtest.TestEntryPoint("foobar", "myep.group", "myep.tests", "test.attr")
+            eptest.TestEntryPoint("foobar", "myep.group", "myep.tests", "test.attr")
         )
 
         package.add_entry_point(
@@ -40,18 +42,18 @@ class Test(unittest.TestCase):
             # [options.entry_points]
             # myep.group = 
             #     foobar2 = myep.tests:test.attr[bar]  
-            packtest.TestEntryPoint("foobar2", "myep.group", "myep.tests", "test.attr", ["bar"])
+            eptest.TestEntryPoint("foobar2", "myep.group", "myep.tests", "test.attr", ["bar"])
         )
         finder.add_package(package)
 
         # ... do tests
-        
+
         # Call this to remove your foobar test package
         finder.remove_package(package)
 
         # Remove all packages
         finder.clear()
-        
+
         # It is necessary to clean up sys.meta_path after your test case with this:
         finder.unregister()
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import importlib.util
-import packtest
+import eptest
 import sys
 
 if importlib.util.find_spec("importlib.metadata"):
@@ -12,7 +12,7 @@ else:
 class TestPackTest(unittest.TestCase):
 
     def test_finder_registration(self):
-        finder = packtest.TestFinder()
+        finder = eptest.TestFinder()
         self.assertNotIn(finder, sys.meta_path)
         finder.register()
         self.assertIn(finder, sys.meta_path)
@@ -20,28 +20,28 @@ class TestPackTest(unittest.TestCase):
         self.assertNotIn(finder, sys.meta_path)
 
     def test_package_registration(self):
-        finder = packtest.TestFinder()
+        finder = eptest.TestFinder()
         finder.register()
-        package = packtest.TestPackage("foobar")
+        package = eptest.TestPackage("foobar")
         finder.add_package(package)
         installed_packages = [x for x in Distribution.discover()]
         self.assertIn(package, installed_packages)
         finder.unregister()
 
     def test_ep_registration(self):
-        finder = packtest.TestFinder()
+        finder = eptest.TestFinder()
         finder.register()
-        package = packtest.TestPackage("foobar")
-        ep = packtest.TestEntryPoint("foo", "foobar", "packtest", "TestFinder", ["foo", "bar"])
+        package = eptest.TestPackage("foobar")
+        ep = eptest.TestEntryPoint("foo", "foobar", "eptest", "TestFinder", ["foo", "bar"])
         package.add_entry_point(ep)
         finder.add_package(package)
         self.assertIn(ep, entry_points(group="foobar"))
         finder.unregister()
 
     def test_finder_clearing(self):
-        finder = packtest.TestFinder()
-        finder.add_package(packtest.TestPackage("foobar"))
-        finder.add_package(packtest.TestPackage("zazz"))
+        finder = eptest.TestFinder()
+        finder.add_package(eptest.TestPackage("foobar"))
+        finder.add_package(eptest.TestPackage("zazz"))
         self.assertIn("foobar", finder._test_packages)
         self.assertIn("zazz", finder._test_packages)
         finder.clear()
@@ -49,9 +49,9 @@ class TestPackTest(unittest.TestCase):
         self.assertNotIn("zazz", finder._test_packages)
 
     def test_finder_remove(self):
-        finder = packtest.TestFinder()
-        finder.add_package(packtest.TestPackage("foobar"))
-        finder.add_package(packtest.TestPackage("zazz"))
+        finder = eptest.TestFinder()
+        finder.add_package(eptest.TestPackage("foobar"))
+        finder.add_package(eptest.TestPackage("zazz"))
         self.assertIn("foobar", finder._test_packages)
         self.assertIn("zazz", finder._test_packages)
         finder.remove_package("foobar")
@@ -59,31 +59,30 @@ class TestPackTest(unittest.TestCase):
         self.assertIn("zazz", finder._test_packages)
 
 
-
 class TestEntryPoint(unittest.TestCase):
 
     def test_load_module(self):
-        ep = packtest.TestEntryPoint("foobar", "foobar", "packtest")
+        ep = eptest.TestEntryPoint("foobar", "foobar", "eptest")
         mod = ep.load()
         self.assertTrue(hasattr(mod, "TestPackage"))
         self.assertTrue(hasattr(mod, "TestEntryPoint"))
         self.assertTrue(hasattr(mod, "__version__"))
 
     def test_load_attribute(self):
-        ep = packtest.TestEntryPoint("foobar", "foobar", "packtest.codepackage", "TestEntryPoint")
+        ep = eptest.TestEntryPoint("foobar", "foobar", "eptest.codepackage", "TestEntryPoint")
         cls = ep.load()
-        self.assertEqual(cls, packtest.TestEntryPoint)
+        self.assertEqual(cls, eptest.TestEntryPoint)
 
     def test_load_subattribute(self):
-        ep = packtest.TestEntryPoint("foobar", "foobar", "packtest.codepackage", "TestEntryPoint.__init__")
+        ep = eptest.TestEntryPoint("foobar", "foobar", "eptest.codepackage", "TestEntryPoint.__init__")
         cls = ep.load()
-        self.assertEqual(cls, packtest.TestEntryPoint.__init__)
+        self.assertEqual(cls, eptest.TestEntryPoint.__init__)
 
     def test_properties(self):
-        ep = packtest.TestEntryPoint("fb", "foobar", "packtest.codepackage", "TestEntryPoint", extras=["foo", "bar"])
+        ep = eptest.TestEntryPoint("fb", "foobar", "eptest.codepackage", "TestEntryPoint", extras=["foo", "bar"])
         self.assertIn("foo", ep.extras)
         self.assertIn("bar", ep.extras)
         self.assertEqual(ep.name, "fb")
         self.assertEqual(ep.group, "foobar")
-        self.assertEqual(ep.module, "packtest.codepackage")
+        self.assertEqual(ep.module, "eptest.codepackage")
         self.assertEqual(ep.attr, "TestEntryPoint")
